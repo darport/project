4984fabedec9e1abe96d0f0b6b87f6796c44fe34
eyalarbib <42712969+eyalarbib@users.noreply.github.com>
2018-09-16 19:36:32 +0300

    documantation

--- ILPSolver.h
+++ ILPSolver.h
@@ -3,10 +3,14 @@
 
 #ifndef ILPSOLVER_H_
 #define ILPSOLVER_H_
-
+/*the function updates the solved board that the struct game keaps, using to sol array that the ilp function returns,
+ *  in order to use it for vary functions*/
 void updateSolved(double *sol,Game *game);
+/*the function releases all the resources used in the ilp function*/
 void freeILP(double *sol, int *ind, int *ind2, double *val, double *val2, double *lb, char *vtype, GRBenv *env,
         GRBmodel *model);
+/*the function uses integer linear programming in order to find a solution for the game board,
+ * returns 1 if a solution was found and 0 o/w*/
 int ILP(Game *game,int command);
 /*
 int createEnvironment(double *sol, int *ind, int *ind2, double *val, double *val2, double *lb, char *vtype, GRBenv *env,
--- Solver.h
+++ Solver.h
@@ -2,13 +2,20 @@
 
 #ifndef SOLVER_H_
 #define SOLVER_H_
-
+/*the function updates the indexes of row and col when moving forword with same solution*/
 void updateIndexForward(int *row, int *col, Game*game);
+/*the function updates the indexes of row and col when moving back to find a different solution*/
 void updateIndexBack(int *row, int *col, Game*game);
+/*the function updates the value of the board[row][col]*/
 void updateValue(Game *game, int *row, int *col, Stack *stk);
+/*the function copying the values of the board to a two dimensional board of ints and returns it*/
 int ** copyBoard(Cell ** board, int size);
+/*the function releases the sources of the temporary int board we use*/
 void freeBoardInt(int **board, int size);
+/*the function restore the game board original values using the copied board of ints*/
 void copyBack(Game *game, int ** copy, int size);
+/*the function uses the backtracking aalgorithm in order to find the amount of solutions possible for the current game board
+ * return the amount of solutions found for the current board*/
 int eBacktracking(Game *game);
 
 #endif /* SOLVER_H_ */
